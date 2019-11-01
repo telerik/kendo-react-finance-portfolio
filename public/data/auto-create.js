0c1f14129b30bbba7b6cfe608f3eb1fc5e07cff9
Kiril Peyanski <peyanski@progress.com>
2019-11-01 23:18:17 +0200

    fix: correct 1D data

--- public/data/auto-create.js
+++ public/data/auto-create.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 // const all = require('./symbols');
 const healthSymbols = require('./health-symbols');
+const techSymbols = require('./tech-symbols');
 const fetch = require('node-fetch');
 const path = require('path');
 const intervals = [
@@ -9,7 +10,7 @@ const intervals = [
     // { name: '30M', minutes: 30 },
     // { name: '1H', minutes: 60 },
     // { name: '4H', minutes: 240 },
-    { name: '1D', minutes: 1440 },
+    // { name: '1D', minutes: 1440 },
     // { name: '1W', minutes: 7 * 1440 },
     // { name: '1M', minutes: 30 * 1440 }
 ]
@@ -31,52 +32,71 @@ const magic = (arg, fixed = 2) => {
         : String((val - dif).toFixed(fixed))
 }
 
-healthSymbols.data.forEach(async (smb) => {
-    await Promise.all(await intervals.map(async (interval) => {
-        // if (fs.existsSync(path.resolve(`./symbols/${smb.symbol}${interval.name}.json`))) {
-        const text = fs.readFileSync(path.resolve(`./symbols/${smb.symbol}.json`), "utf-8");
+healthSymbols.data.concat(techSymbols.data).forEach(async (smb) => {
+    if (fs.existsSync(path.resolve(`./symbols/${smb.symbol}5M.json`))) {
+        console.log("try", smb.symbol);
+        const text = fs.readFileSync(path.resolve(`./symbols/${smb.symbol}5M.json`), "utf-8");
         if (text) {
             const obj = JSON.parse(text);
-            const intra = {};
-            const foo = MS_PER_MINUTE * interval.minutes;
-            const keys = Object.keys(obj.intraday);
-            const init = obj.intraday[keys[0]];
-
-            for (let i = rangeStart.getTime(); i <= rangeEnd.getTime(); i += foo) {
-                const old = intra[new Date(i - foo).toISOString()] !== undefined
-                    ? intra[new Date(i - foo).toISOString()]
-                    : init;
-
-                const rnd = (Math.random() + 0.01);
-                const volatility = 0.03;
-                let cngP = 2 * volatility * rnd;
-                if (cngP > volatility) {
-                    cngP -= (2 * volatility);
-                }
-                const change = Number(old.close) * cngP;
-                const newPrice = Number(old.close) + change;
-                const high = Math.max(newPrice, Number(old.close));
-                const low = Math.min(newPrice, Number(old.close));
-
-                intra[new Date(i).toISOString()] = {
-                    open: String((Number.parseFloat(old.close) + 0.01).toFixed(2)),
-                    close: String(newPrice.toFixed(2)),
-                    high: String((high + (0.015 * high)).toFixed(2)),
-                    low: String((low - (0.015 * low)).toFixed(2)),
-                    volume: magic(old.volume, 0),
-                }
-            }
-
-            obj.intraday = intra;
-
-            fs.writeFileSync(path.resolve(`./symbols/${smb.symbol}${interval.name}.json`), JSON.stringify(obj));
-            console.log('success', smb.symbol, interval.name);
-        } else {
-            console.warn("WARN", smb.symbol, interval);
+            const intra = obj.intraday;
+            const keys = Object.keys(intra);
+            const lastDay = keys.slice(keys.length - 289, keys.length - 1).reduce((acc, current) => {
+                acc[current] = intra[current];
+                return acc
+            }, {});
+            obj.intraday = lastDay;
+            fs.writeFileSync(path.resolve(`./symbols/${smb.symbol}1D.json`), JSON.stringify(obj));
+            console.log("succes", smb.symbol);
         }
-        // }
-    }))
+    }
 })
+
+// healthSymbols.data.forEach(async (smb) => {
+//     await Promise.all(await intervals.map(async (interval) => {
+//         // if (fs.existsSync(path.resolve(`./symbols/${smb.symbol}${interval.name}.json`))) {
+//         const text = fs.readFileSync(path.resolve(`./symbols/${smb.symbol}.json`), "utf-8");
+//         if (text) {
+//             const obj = JSON.parse(text);
+//             const intra = {};
+//             const foo = MS_PER_MINUTE * interval.minutes;
+//             const keys = Object.keys(obj.intraday);
+//             const init = obj.intraday[keys[0]];
+
+//             for (let i = rangeStart.getTime(); i <= rangeEnd.getTime(); i += foo) {
+//                 const old = intra[new Date(i - foo).toISOString()] !== undefined
+//                     ? intra[new Date(i - foo).toISOString()]
+//                     : init;
+
+//                 const rnd = (Math.random() + 0.01);
+//                 const volatility = 0.03;
+//                 let cngP = 2 * volatility * rnd;
+//                 if (cngP > volatility) {
+//                     cngP -= (2 * volatility);
+//                 }
+//                 const change = Number(old.close) * cngP;
+//                 const newPrice = Number(old.close) + change;
+//                 const high = Math.max(newPrice, Number(old.close));
+//                 const low = Math.min(newPrice, Number(old.close));
+
+//                 intra[new Date(i).toISOString()] = {
+//                     open: String((Number.parseFloat(old.close) + 0.01).toFixed(2)),
+//                     close: String(newPrice.toFixed(2)),
+//                     high: String((high + (0.015 * high)).toFixed(2)),
+//                     low: String((low - (0.015 * low)).toFixed(2)),
+//                     volume: magic(old.volume, 0),
+//                 }
+//             }
+
+//             obj.intraday = intra;
+
+//             fs.writeFileSync(path.resolve(`./symbols/${smb.symbol}${interval.name}.json`), JSON.stringify(obj));
+//             console.log('success', smb.symbol, interval.name);
+//         } else {
+//             console.warn("WARN", smb.symbol, interval);
+//         }
+//         // }
+//     }))
+// })
 
 // healthSymbols.data.forEach(async (smb) => {
 //     if (fs.existsSync(path.resolve(`./symbols/${smb.symbol}.json`))) {
