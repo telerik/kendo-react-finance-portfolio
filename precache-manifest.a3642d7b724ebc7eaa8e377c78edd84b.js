21a3472561d55a1e175ad4124568fe4078af9761
Kiril Peyanski <peyanski@progress.com>
2019-11-02 19:13:56 +0200

    Updates

---
+++ precache-manifest.a3642d7b724ebc7eaa8e377c78edd84b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c78d74eef827f1ce220a20573c86bc",
+    "url": "/kendo-react-finance-portfolio/index.html"
+  },
+  {
+    "revision": "60b88b36728144f40e9a",
+    "url": "/kendo-react-finance-portfolio/static/css/main.96701605.chunk.css"
+  },
+  {
+    "revision": "55a4fb6fca259986319c",
+    "url": "/kendo-react-finance-portfolio/static/js/2.86b4f697.chunk.js"
+  },
+  {
+    "revision": "60b88b36728144f40e9a",
+    "url": "/kendo-react-finance-portfolio/static/js/main.472d98c0.chunk.js"
+  },
+  {
+    "revision": "25e4b4551d7b78009ab4",
+    "url": "/kendo-react-finance-portfolio/static/js/runtime-main.7a63929c.js"
+  },
+  {
+    "revision": "2e780390bdf45b7abb6529d6b6f0606c",
+    "url": "/kendo-react-finance-portfolio/static/media/area.2e780390.svg"
+  },
+  {
+    "revision": "f5c64e3c6b63320f089d473eb176fb74",
+    "url": "/kendo-react-finance-portfolio/static/media/candle.f5c64e3c.svg"
+  },
+  {
+    "revision": "a08c1749b859592b8bc40530a7ffbdba",
+    "url": "/kendo-react-finance-portfolio/static/media/footer-bg.a08c1749.svg"
+  },
+  {
+    "revision": "fb71804a4fb1923f2360481179e8098b",
+    "url": "/kendo-react-finance-portfolio/static/media/header-bg.fb71804a.svg"
+  },
+  {
+    "revision": "1ce98a430f5b117dff20121222a2ebba",
+    "url": "/kendo-react-finance-portfolio/static/media/line.1ce98a43.svg"
+  },
+  {
+    "revision": "30f34c6e5c421dc64e989c4c73dc4bf3",
+    "url": "/kendo-react-finance-portfolio/static/media/user.30f34c6e.jpg"
+  }
+]);
