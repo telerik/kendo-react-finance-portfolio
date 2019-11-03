a735f84d1435cd385b472198a6a15b5fb8b5c066
Kiril Peyanski <peyanski@progress.com>
2019-11-03 15:13:12 +0200

    Updates

---
+++ precache-manifest.04d1e27a7d85cd7449fc42b5b0304510.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88adca219a9f795f304495aaeb0ed514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9607aaf7f2ebab9d635b",
+    "url": "/static/css/main.33b006ab.chunk.css"
+  },
+  {
+    "revision": "1c5bce63df7b74cbd3bc",
+    "url": "/static/js/2.f75b0104.chunk.js"
+  },
+  {
+    "revision": "9607aaf7f2ebab9d635b",
+    "url": "/static/js/main.5c9d4a29.chunk.js"
+  },
+  {
+    "revision": "2c9f459426ee206af1d2",
+    "url": "/static/js/runtime-main.e4ac5e05.js"
+  },
+  {
+    "revision": "2e780390bdf45b7abb6529d6b6f0606c",
+    "url": "/static/media/area.2e780390.svg"
+  },
+  {
+    "revision": "f5c64e3c6b63320f089d473eb176fb74",
+    "url": "/static/media/candle.f5c64e3c.svg"
+  },
+  {
+    "revision": "a08c1749b859592b8bc40530a7ffbdba",
+    "url": "/static/media/footer-bg.a08c1749.svg"
+  },
+  {
+    "revision": "fb71804a4fb1923f2360481179e8098b",
+    "url": "/static/media/header-bg.fb71804a.svg"
+  },
+  {
+    "revision": "1ce98a430f5b117dff20121222a2ebba",
+    "url": "/static/media/line.1ce98a43.svg"
+  },
+  {
+    "revision": "30f34c6e5c421dc64e989c4c73dc4bf3",
+    "url": "/static/media/user.30f34c6e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kendo-react-finance-portfolio/precache-manifest.bb5eafdf85e1caacca76daabaad475e8.js"
+  "/precache-manifest.04d1e27a7d85cd7449fc42b5b0304510.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kendo-react-finance-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
