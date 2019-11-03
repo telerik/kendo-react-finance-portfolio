70fe5b46d9c8076d7de80fbf492544835cadaabe
Kiril Peyanski <peyanski@progress.com>
2019-11-03 15:11:22 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kendo-react-finance-portfolio/precache-manifest.f2ffda3f5055dd070d1a30ec8c9f3f3d.js"
+  "/kendo-react-finance-portfolio/precache-manifest.bb5eafdf85e1caacca76daabaad475e8.js"
 );
 
 self.addEventListener('message', (event) => {
